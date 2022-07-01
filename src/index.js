7d9aead71c01c8d1367df6bcfd45f5b80869222c
Samantha Wu <samanthayywu@gmail.com>
2022-07-01 15:25:00 +0100

    plan

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
+
+// REE- get weight, height, age, gender
+
+// TDEE- sedentary, light, moderate, v active
+
+// for weight loss or gains?
