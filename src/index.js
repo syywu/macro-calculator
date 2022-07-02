ec3cb5bf659fb0cbc62d3f90745c4164daa6af9e
Samantha Wu <samanthayywu@gmail.com>
2022-07-02 13:01:50 +0100

    askWeight and askHeight

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
 
+let female;
+let male;
+let inputWeight;
+let inputHeight;
+let inputAge;
+await askWeight();
+await askHeight();
+
 // REE- get weight, height, age, gender
+async function askWeight() {
+  const weight = await inquirer.prompt({
+    name: "weight",
+    type: "input",
+    message: "Please enter your weight in kg",
+  });
+  inputWeight = weight.inputWeight;
+}
+
+async function askHeight() {
+  const height = await inquirer.prompt({
+    name: "height",
+    type: "input",
+    message: "Please enter your height in cm",
+  });
+  inputHeight = height.inputHeight;
+}
 
 // 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5 = REE- MEN
 // 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) – 161 = REE- WOMEN
@@ -14,11 +39,9 @@ import { createSpinner } from "nanospinner";
 
 /*
 Daily protein calories
-
 When already lean and lifting heavy for bodybuilding use a measure of 1 g of protein per pound of body weight.
 Most people can use a more balanced approach and use .825 g protein per pound since most people are carrying fat tissue which skews the “1 gram per pound rule.”
 For people with a lot of excess fat weight or people who don’t do a lot of strength training, use .65 grams per pound of body weight.
-
 */
 
 /*
